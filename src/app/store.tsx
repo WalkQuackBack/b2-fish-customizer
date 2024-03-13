7db294f79572819a6b0e2a33020799741fc16d1a
WalkQuackBack <163211185+WalkQuackBack@users.noreply.github.com>
2024-03-12 20:42:40 -0600

    Push github-pages

--- src/app/store.tsx
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import fishDataReducer from "./features/fishData/fishDataSlice";
-
-export default configureStore({
-  reducer: {
-    fishData: fishDataReducer,
-  },
-});
